1a86a83ca4379a9eefaf7f2561ad6134cdf4bdc7
Hacking-NASSA-with-HTML <follow.trend7777@gmail.com>
2022-12-26 00:13:13 +0400

    feat: add another site name

--- assets/click.js
+++ assets/click.js
@@ -1,7 +1,7 @@
 let myWindow;
 
 function openWin() {
-    myWindow = window.open("https://render-back-end-nikky-pedia.onrender.com/", "", "width=500, height=500");
+    myWindow = window.open("https://www.w3schools.com", "name", "width=500, height=500");
 }
 
 function closeWin() {
@@ -9,7 +9,7 @@ function closeWin() {
     // myWindow = window.close("https://www.w3schools.com", "_blank", "width=500, height=500");
 }
 
-
+//https://render-back-end-nikky-pedia.onrender.com/
 // (function cycle() {
 //     document.getElementById('open').click()
 //     console.log("Button is clicked")

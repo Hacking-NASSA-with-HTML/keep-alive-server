c38b9104367bc2295675c84a7a59e1edbc32e5b0
Hacking-NASSA-with-HTML <follow.trend7777@gmail.com>
2022-12-28 20:39:40 +0400

    feat: add styling to main button

--- assets/js/myScript.js
+++ assets/js/myScript.js
@@ -14,7 +14,7 @@ function mainPageContent() {
 
                 <button id="open" onclick="openWin()">Open "myWindow"</button>
                 <button id="close" onclick="closeWin()">Close "myWindow"</button>
-                <div><button id="infiniteRun"></button></div>
+                <div><button id="infiniteRun" class="styled"></button></div>
             </div>
         `
     return renderMainPageContent
